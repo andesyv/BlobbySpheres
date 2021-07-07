c85f765cc1a2865513b4edd11ea06f26c9b57ea2
Anders Syvertsen <andesyv@gmail.com>
2021-07-08 00:16:26 +0200

    Added global defines to shaders
    Defines can now be passed in with shaders to add global constants to shader code

---
+++ src/shader.cpp
@@ -0,0 +1,135 @@
+#include "shader.h"
+#include <format>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <sstream>
+
+Shader::SubShader::SubShader(int _id, const std::string& path)
+ : id{_id}, filePath{path}
+{}
+
+Shader::SubShader::SubShader(SubShader&& lhs)
+ : id{lhs.id} {
+    lhs.bOwned = false;
+}
+
+Shader::SubShader& Shader::SubShader::operator=(SubShader&& lhs) {
+    id = lhs.id;
+    lhs.bOwned = false;
+    return *this;
+}
+
+Shader::SubShader::~SubShader() {
+    if (bOwned)
+        glDeleteShader(id);
+}
+
+
+
+
+std::optional<std::pair<GLenum, int>> Shader::createSubShader(const std::pair<GLenum, std::string>& program, const std::string& programDefines) {
+    const auto& [type, relPath] = program;
+    const auto path = std::string{SHADER_BASE_PATH}.append(relPath);
+
+    // Read file:
+    std::ifstream input{path, std::ifstream::in | std::ifstream::ate};
+    if (!input)
+    {
+        std::cout << "SHADER ERROR: Path " << path << " not found." << std::endl;
+        return {};
+    }
+
+    std::string source{};
+    const auto n = static_cast<std::size_t>(input.tellg());
+    source.reserve(n + programDefines.size());
+    input.seekg(0, input.beg);
+    // 1. Read first few #define lines in shader
+    for (std::string line; input && input.peek() == '#';) {
+        std::getline(input, line);
+        source += line + '\n';
+    }
+    // 2. Add defines:
+    source += programDefines;
+    // 3. Read rest of file:
+    source.insert(source.end(), std::istreambuf_iterator<char>{input}, std::istreambuf_iterator<char>{});
+    auto sourcePtr = source.c_str();
+    input.close();
+
+    // Compile shader:
+    int prog = glCreateShader(type);
+    glShaderSource(prog, 1, &sourcePtr, NULL);
+    glCompileShader(prog);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(prog, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(prog, 512, NULL, infoLog);
+        std::cout << "SHADER COMPILATION ERROR in file: \"" << path << "\"\n"
+                    << infoLog << std::endl;
+        return std::nullopt;
+    }
+
+    return std::make_pair(type, prog);
+}
+
+// Compiles and appends a program to this shaders list of subshaders. Returns true if new shader was added.
+bool Shader::addShader(const std::pair<GLenum, std::string>& program) {
+    auto result = createSubShader(program, getDefineStr());
+    if (!result)
+        return false;
+
+    auto [key, value] = *result;
+    programs.insert(std::move(std::make_pair(key, std::move(SubShader{value, program.second}))));
+    bValid = false;
+    return true;
+}
+
+void Shader::addDefine(const std::string& value) {
+    defines.insert(value);
+    bValid = false;
+}
+
+void Shader::addDefine(std::string&& value) {
+    defines.insert(std::move(value));
+    bValid = false;
+}
+
+std::string Shader::getDefineStr() const {
+    std::string output{};
+    for (const auto& s : defines)
+        output.append(std::format("#define {}\n", s));
+    return output;
+}
+
+// Attempts to link this shaers subshaders. Returns true if successfull. 
+bool Shader::link() {
+    id = glCreateProgram();
+    for (const auto& prog : programs)
+        glAttachShader(id, prog.second.id);
+        
+    glLinkProgram(id);
+    // check for linking errors
+    int success;
+    char infoLog[512];
+    glGetProgramiv(id, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(id, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                    << infoLog << std::endl;
+        return false;
+    }
+    
+    bValid = true;
+    return true;
+}
+
+Shader::~Shader() {
+    programs.clear();
+
+    // Delete program:
+    glDeleteProgram(id);
+}
--- src/shader.h
+++ src/shader.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <string>
-#include <fstream>
-#include <sstream>
 #include <iostream>
 #include <optional>
 #include <map>
+#include <set>
+#include <array>
+#include <string_view>
+
 #include <glad/glad.h>
+
 #include "constants.h"
 
 class Shader
@@ -14,27 +17,20 @@ class Shader
 public:
     struct SubShader {
         int id;
+        std::string filePath;
     
     private:
         bool bOwned = true;
         
     public:
-        SubShader(int _id) : id{_id} {}
+        SubShader(int _id, const std::string& path);
         SubShader(const SubShader&) = default;
-        SubShader(SubShader&& lhs) : id{lhs.id} {
-            lhs.bOwned = false;
-        }
+        SubShader(SubShader&& lhs);
 
         SubShader& operator=(const SubShader&) = default;
-        SubShader& operator=(SubShader&& lhs) {
-            id = lhs.id;
-            lhs.bOwned = false;
-        }
+        SubShader& operator=(SubShader&& lhs);
 
-        ~SubShader() {
-            if (bOwned)
-                glDeleteShader(id);
-        }
+        ~SubShader();
     };
 
 private:
@@ -42,80 +38,22 @@ private:
     int id;
 
     std::map<GLenum, SubShader> programs;
+    std::set<std::string> defines;
 
 public:
-    static std::optional<std::pair<GLenum, int>> createSubShader(const std::pair<GLenum, std::string>& program) {
-        const auto& [type, relPath] = program;
-        const auto path = std::string{SHADER_BASE_PATH}.append(relPath);
+    // Reads a program from file and compiles it to a subshader. Returns subshader if successful.
+    static std::optional<std::pair<GLenum, int>> createSubShader(const std::pair<GLenum, std::string>& program, const std::string& programDefines = "");
 
-        // Read file:
-        std::ifstream input{path, std::ifstream::in | std::ifstream::ate};
-        if (!input)
-        {
-            std::cout << "SHADER ERROR: Path " << path << " not found." << std::endl;
-            return {};
-        }
+    // Compiles and appends a program to this shaders list of subshaders. Returns true if new shader was added.
+    bool addShader(const std::pair<GLenum, std::string>& program);
 
-        std::string source{};
-        source.reserve(input.tellg());
-        input.seekg(0, input.beg);
-        source.insert(source.begin(), std::istreambuf_iterator<char>{input}, std::istreambuf_iterator<char>{});
-        auto sourcePtr = source.c_str();
-        input.close();
+    void addDefine(const std::string& value);
+    void addDefine(std::string&& value);
 
-        // Compile shader:
-        int prog = glCreateShader(type);
-        glShaderSource(prog, 1, &sourcePtr, NULL);
-        glCompileShader(prog);
-        // check for shader compile errors
-        int success;
-        char infoLog[512];
-        glGetShaderiv(prog, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(prog, 512, NULL, infoLog);
-            std::cout << "SHADER COMPILATION ERROR in file: \"" << path << "\"\n"
-                      << infoLog << std::endl;
-            return std::nullopt;
-        }
-
-        return std::make_pair(type, prog);
-    }
-
-    // Appends a program to this shaders list of subshaders. Returns true if new shader was added.
-    bool addShader(const std::pair<GLenum, std::string>& program) {
-        auto result = createSubShader(program);
-        if (!result)
-            return false;
-
-        auto [key, value] = *result;
-        programs.insert(std::move(std::make_pair(key, std::move(SubShader{value}))));
-        bValid = false;
-        return true;
-    }
+    std::string getDefineStr() const;
 
     // Attempts to link this shaers subshaders. Returns true if successfull. 
-    bool link() {
-        id = glCreateProgram();
-        for (const auto& prog : programs)
-            glAttachShader(id, prog.second.id);
-            
-        glLinkProgram(id);
-        // check for linking errors
-        int success;
-        char infoLog[512];
-        glGetProgramiv(id, GL_LINK_STATUS, &success);
-        if (!success)
-        {
-            glGetProgramInfoLog(id, 512, NULL, infoLog);
-            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
-                      << infoLog << std::endl;
-            return false;
-        }
-        
-        bValid = true;
-        return true;
-    }
+    bool link();
 
     template <std::size_t I>
     void compileAndLink(std::array<std::pair<GLenum, std::string>, I>&& params) {
@@ -127,13 +65,30 @@ public:
         link();
     }
 
+    template <std::size_t I, std::size_t J>
+    void compileAndLink(std::array<std::pair<GLenum, std::string>, I>&& params, std::array<std::string_view, J>&& globalDefines) {
+        for (auto def : globalDefines)
+            addDefine(std::string{def});
+        
+        for (auto param : params) {
+            if (!addShader(param))
+                return;
+        }
+
+        link();
+    }
+
     template <std::size_t I>
     Shader(std::pair<GLenum, std::string>(&& params)[I]) {
-        std::array<std::pair<GLenum, std::string>, I> arr;
-        for (auto i{0}; i < I; ++i)
-            arr[i] = params[i];
-        compileAndLink(std::move(arr));
-        // compileAndLink(std::to_array<std::pair<GLenum, std::string>, I>(params)); // C++20
+        compileAndLink(std::to_array<std::pair<GLenum, std::string>, I>(params)); // C++20
+    }
+
+    template <std::size_t I, std::size_t J>
+    Shader(std::pair<GLenum, std::string>(&& params)[I], std::string_view (&& globalDefines)[J]) {
+        compileAndLink(
+            std::to_array<std::pair<GLenum, std::string>, I>(params),
+            std::to_array<std::string_view, J>(globalDefines)
+        );
     }
 
     // template <typename ... S>
@@ -141,13 +96,9 @@ public:
     //     compileAndLink<sizeof...(S)>(std::to_array<std::pair<GLenum, std::string>, sizeof...(S)>(static_cast<std::pair<GLenum, std::string>>(args) ...));
     // }
 
+    bool valid() const { return bValid; }
     int get() const { return id; }
     int operator* () const { return get(); }
 
-    ~Shader() {
-        programs.clear();
-
-        // Delete program:
-        glDeleteProgram(id);
-    }
+    ~Shader();
 };
